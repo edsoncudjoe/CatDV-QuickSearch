b99d90b8fe7e2f5b7d206b4a9879b75f465654f6
sp00f <e.cudjoe@hotmail.co.uk>
2015-04-19 03:00:37 +0100

    wrote GUI into a class. Made the GUI resizeable. Removed select and export features.

--- CatDV Quick Search.py
+++ CatDV Quick Search.py
@@ -5,6 +5,7 @@ import tkMessageBox
 import requests
 import json
 import logging
+import tkFont
 
 sys.path.insert(0, '../py-catdv') # IV local
 sys.path.insert(0, '../Py-CatDV')
@@ -19,7 +20,7 @@ logger = logging.getLogger(__name__)
 cdv = Catdvlib()
 
 #external access
-#cdv.url = "http://mam.intervideo.co.uk:8080/api/4"
+cdv.url = "http://mam.intervideo.co.uk:8080/api/4"
 
 def c_login():
 	try:
@@ -63,8 +64,8 @@ def query():
 				try:
 					if i['userFields']['U7']:
 						count += 1
-						app.result.insert(END, i['userFields']['U7'] + '    ' + 
-							i['name'])       
+						app.result.insert(END, i['userFields']['U7'] + ' '*5 + 
+							i['name'] + ' '*7 + '*' + i['notes'])       
 					else:
 						count += 1
 						app.result.insert(END, i['name'])
@@ -108,34 +109,39 @@ def about():
 	tkMessageBox.showinfo("CatDV QuickSearch 1.0b",
 		"\nCatDV QuickSearch\n"
 		"\nCreated by E.Cudjoe"
-		"\nVersion 1.0b")
+		"\nVersion 1.0.1b"
 		#"\nCopyright " + "\u00A9" + " 2014-2015 E.cudjoe"
-		#"\nhttps://github.com/edsondudjoe")
+		"\nhttps://github.com/edsondudjoe")
+
+# Tkinter grid maanagement and listbox commands.
 N = tk.N
 S = tk.S
 E = tk.E
 W = tk.W
 END = tk.END
 
+
+
 class QS(tk.Frame):
 	def __init__(self, parent):
 		tk.Frame.__init__(self, parent)
 		self.parent = parent
-		self.parent.columnconfigure(0, weight=1)
-		self.parent.rowconfigure(0, weight=1)
-		self.parent.config(bg="tan2")
-		self.login_frame = tk.Frame(parent, bg="red")
-		self.search_frame = tk.Frame(parent, bg="yellow")
-		self.result_frame = tk.Frame(parent, bg="green")
-		self.bottom_frame = tk.Frame(parent, bg="blue")
-		
-		self.login_frame.grid(sticky=W, padx=5, pady=5)
-		self.search_frame.grid(sticky=W, padx=5, pady=5)
-		self.result_frame.grid(sticky=W, padx=5, pady=5)
-		self.bottom_frame.grid(sticky=S+E, padx=5, pady=5)
 
-		self.result_frame.rowconfigure(0, weight=1)
-		self.result_frame.columnconfigure(0, weight=2)		
+		self.parent.columnconfigure(0, weight=4)
+		self.parent.rowconfigure(2, weight=4)
+		
+		self.login_frame = tk.Frame(parent)
+		self.search_frame = tk.Frame(parent)
+		self.result_frame = tk.Frame(parent)
+		self.bottom_frame = tk.Frame(parent)
+		
+		self.login_frame.grid(row=0, sticky=W+E, padx=5, pady=10)
+		self.search_frame.grid(row=1, sticky=W+E, padx=5, pady=10)
+		self.result_frame.grid(row=2, sticky=N+S+W+E, padx=5, pady=10)
+		self.bottom_frame.grid(row=3, sticky=S+E, padx=5, pady=10)
+
+		self.result_frame.rowconfigure(0, weight=4)
+		self.result_frame.columnconfigure(0, weight=4)		
 
 		self.create_menubar()
 		self.create_variables()
@@ -154,22 +160,22 @@ class QS(tk.Frame):
 		self.filemenu.add_command(label="Login", command=c_login)
 		self.filemenu.add_command(label="Search", command=query)
 		# File > Export
-		self.exportmenu = tk.Menu(self.filemenu, tearoff=0)
-		self.filemenu.add_cascade(label="Export", menu=self.exportmenu,
-			state="disabled")
-		self.exportmenu.add_command(label="as Spreadsheet")
-		self.exportmenu.add_command(label="as Text")
+		#self.exportmenu = tk.Menu(self.filemenu, tearoff=0)
+		#self.filemenu.add_cascade(label="Export", menu=self.exportmenu,
+		#	state="disabled")
+		#self.exportmenu.add_command(label="as Spreadsheet")
+		#self.exportmenu.add_command(label="as Text")
 		# File
-		self.filemenu.add_command(label="Print", state="disabled")
+		#self.filemenu.add_command(label="Print", state="disabled")
 		self.filemenu.add_command(label="Clear", command=clear_text)
 		self.filemenu.add_command(label="Logout", command=delete_session)
 		self.filemenu.add_command(label="Quit", command=root.quit)
 
 		# Edit
-		self.editmenu = tk.Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="Edit", menu=self.editmenu)
-		self.editmenu.add_command(label="Select All", state="disabled")
-		self.editmenu.add_command(label="Copy", state="disabled")
+		#self.editmenu = tk.Menu(self.menubar, tearoff=0)
+		#self.menubar.add_cascade(label="Edit", menu=self.editmenu)
+		#self.editmenu.add_command(label="Select All", state="disabled")
+		#self.editmenu.add_command(label="Copy", state="disabled")
 
 		# Help
 		self.helpmenu = tk.Menu(self.menubar, tearoff=0)
@@ -196,10 +202,14 @@ class QS(tk.Frame):
 		self.clip.bind("<Return>", enter_query)
 		self.search_btn = ttk.Button(self.search_frame, text="SEARCH", command=query)
 		
+		m_font = tkFont.Font(root=self.result_frame, size=14)
 		self.scrollbar = ttk.Scrollbar(self.result_frame)
-		self.result = tk.Listbox(self.result_frame, bg='grey', width=100, height=30)
-		self.result.config(yscrollcommand=self.scrollbar.set)
+		self.u_scrollbar = ttk.Scrollbar(self.result_frame, orient='horizontal')
+		self.result = tk.Listbox(self.result_frame, bg='grey', width=100, height=20)
+		self.result.config(xscrollcommand = self.u_scrollbar.set,
+			yscrollcommand=self.scrollbar.set, font=m_font)
 		self.scrollbar.config(command=self.result.yview)
+		self.u_scrollbar.config(command=self.result.xview)
 		
 		self.clr_btn = ttk.Button(self.bottom_frame, text="Clear", command=clear_text)
 		self.quit_button = ttk.Button(self.bottom_frame, text="QUIT", command=root.quit)
@@ -213,9 +223,10 @@ class QS(tk.Frame):
 		self.logout_btn.grid(row=0, column=5, padx=2)
 
 		self.clip.grid(row=0, column=0, columnspan=4, sticky=E, padx=2) #colspan
-		self.search_btn.grid(row=0, column=1, sticky=E, padx=2)
-		self.scrollbar.grid(column=1, sticky=N+S+W)
-		self.result.grid(row=0, column=0, columnspan=4)
+		self.search_btn.grid(row=0, column=5, sticky=E, padx=2)
+		self.scrollbar.grid(column=5, sticky=N+S+W)
+		self.u_scrollbar.grid(row=1, sticky=E+W+S)
+		self.result.grid(row=0, column=0, columnspan=4, sticky=N+S+E+W)
 		
 		self.clr_btn.grid(row=0, column=0, sticky=E, pady=2, padx=2)
 		self.quit_button.grid(row=0, column=1, sticky=E, pady=2, padx=2)
@@ -225,7 +236,8 @@ class QS(tk.Frame):
 
 root = tk.Tk()
 root.title('CatDV QuickSearch')
-root.geometry('500x800+200+100')
+#root.update()
+root.minsize(root.winfo_width(), root.winfo_height())
 
 app = QS(root)
 
