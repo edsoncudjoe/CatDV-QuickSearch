11c80abf1b74101e191d627b7de152c025033ed6
sp00f <e.cudjoe@hotmail.co.uk>
2015-06-16 11:47:01 +0100

    Replaced listbox with tree view. Moved listbox to top right to display log results. Need to complete right click menu.

--- CatDV Quick Search.py
+++ CatDV Quick Search.py
@@ -67,14 +67,20 @@ def query():
 					if i['userFields']['U7']:
 						count += 1
 						if i['notes']:
-							s_results.append((i['userFields']['U7'], i['name'], \
-								i['notes']))
-							app.result.insert(END, i['userFields']['U7'] + 
-								' '*5 + i['name'] + ' '*7 + '*' + i['notes'])
+							s_results.append((i['userFields']['U7'], \
+								i['name'], i['notes']))
+#							app.result.insert(END, i['userFields']['U7'] + 
+#								' '*5 + i['name'] + ' '*7 + '*' + i['notes'])
+							app.tree.insert("", count, text=str(count), \
+								values=(i['userFields']['U7'], i['name'], \
+									i['notes']))
 						else:
-							s_results.append((i['userFields']['U7'], i['name']))       
-							app.result.insert(END, 
-								i['userFields']['U7'] + ' '*5 + i['name'])
+							s_results.append((i['userFields']['U7'], \
+								i['name']))       
+#							app.result.insert(END, 
+#								i['userFields']['U7'] + ' '*5 + i['name'])
+							app.tree.insert("", count, text=str(count), \
+								values=(i['userFields']['U7'], i['name']))
 					else:
 						count += 1
 						app.result.insert(END, i['name'])
@@ -102,9 +108,6 @@ def enter_login(event):
 	c_login()
 	return
 
-
-
-
 def export_text():
 	"""
 	Once the user has defined a filename. This function will export
@@ -117,6 +120,7 @@ def export_text():
 			for l in range(len(r)):
 				user_results.write((r[l] + '\t' + '\n'))
 		user_results.write('\n')
+	#tkMessageBox.showinfo("Saved", "File has been saved")
 
 def clear_text():
 	app.result.delete(0, END)
@@ -135,9 +139,14 @@ def about():
 	tkMessageBox.showinfo("CatDV QuickSearch 1.0b",
 		"\nCatDV QuickSearch\n"
 		"\nCreated by E.Cudjoe"
-		"\nVersion 1.0.1b"
-		"\nCopyright " + "\u00A9" + " 2014-2015 E.cudjoe"
-		"\nhttps://github.com/edsondudjoe")
+		"\nVersion 1.0.1b")
+		#"\nCopyright " + "\u00A9" + " 2014-2015 E.cudjoe"
+		#"\nhttps://github.com/edsondudjoe")
+
+def select_all(event):
+	tree_items = app.tree.identify_row(event.y)
+	print app.tree.item(item)
+
 
 # Tkinter grid management and listbox commands.
 N = tk.N
@@ -145,8 +154,6 @@ S = tk.S
 E = tk.E
 W = tk.W
 END = tk.END
-
-
 
 class QS(tk.Frame):
 	def __init__(self, parent):
@@ -167,21 +174,24 @@ class QS(tk.Frame):
 		self.result_frame.grid(row=2, sticky=N+S+W+E, padx=5, pady=10)
 		self.bottom_frame.grid(row=3, sticky=S+E, padx=5, pady=10)
 
+		#self.login_frame.rowconfigure(7, weight=1)
+		self.login_frame.columnconfigure(6, weight=1)
 		self.result_frame.rowconfigure(0, weight=4)
 		self.result_frame.columnconfigure(0, weight=4)
 
 		self.create_menubar()
-		self.create_right_click()
+#		self.create_right_click()
 		self.create_variables()
 		self.create_widgets()
 		self.grid_widgets()
 
-	
-
 	######## MENU BAR #########
 	def create_menubar(self):
 
-		self.menubar = tk.Menu(self.master)
+		self.a = 1
+		self.b = 2
+
+		self.menubar = tk.Menu(self.parent)
 		self.filemenu = tk.Menu(self.menubar, tearoff=0,)
 		root.config(menu=self.menubar)
 
@@ -193,7 +203,6 @@ class QS(tk.Frame):
 		self.exportmenu = tk.Menu(self.filemenu, tearoff=0)
 		self.filemenu.add_cascade(label="Export", menu=self.exportmenu)
 		#self.exportmenu.add_command(label="as Spreadsheet")
-
 		self.exportmenu.add_command(label="as Text", command=export_text)
 
 		# File
@@ -212,17 +221,17 @@ class QS(tk.Frame):
 		self.menubar.add_cascade(label="Help", menu=self.helpmenu)
 		self.helpmenu.add_command(label="About", command=about)
 
-	def create_right_click(self):
-		# Right-click menu
-		self.rc = tk.Menu(self.parent, tearoff=0)
-		self.rc.add_command(label="Select All")
-		self.rc.add_command(label="Copy")
-
-	def r_click(self, event):
-		try:
-			self.rc.tk_popup(event.x_root, event.y_root, 0)
-		finally:
-			self.rc.grab_release()	
+#	def create_right_click(self):
+#		# Right-click menu
+#		self.rc = tk.Menu(self.parent, tearoff=0)
+#		self.rc.add_command(label="Select All", command=select_all)
+#		self.rc.add_command(label="Copy")
+#
+#	def r_click(self, event):
+#		try:
+#			self.rc.tk_popup(event.x_root, event.y_root, 0)
+#		finally:
+#			self.rc.grab_release()	
 
 	def create_variables(self):
 		self.username = tk.StringVar() 
@@ -231,34 +240,56 @@ class QS(tk.Frame):
 
 	def create_widgets(self):
 		self.usrname_label = ttk.Label(self.login_frame, text="Username: ")
-		self.usrname_entry = ttk.Entry(self.login_frame, textvariable=self.username)
+		self.usrname_entry = ttk.Entry(self.login_frame, \
+			textvariable=self.username)
 		self.password_label = ttk.Label(self.login_frame, text="Password: ")
 		self.password_entry = ttk.Entry(
 			self.login_frame, textvariable=self.password,show="*")
 		self.password_entry.bind("<Return>", enter_login)
-		self.login_btn = ttk.Button(self.login_frame, text="LOGIN", command=c_login)
+		self.login_btn = ttk.Button(self.login_frame, text="LOGIN", \
+			command=c_login)
 		self.login_btn.bind("<Return>", enter_login)
 		self.logout_btn = ttk.Button(self.login_frame, text="LOG OUT",
 			command=delete_session)
 
-		self.clip = ttk.Entry(self.search_frame, width="90", textvariable=self.term)
+		self.clip = ttk.Entry(self.search_frame, width="90", \
+			textvariable=self.term)
 		self.clip.bind("<Return>", enter_query)
-		self.search_btn = ttk.Button(self.search_frame, text="SEARCH", command=query)
+		self.search_btn = ttk.Button(self.search_frame, text="SEARCH", \
+			command=query)
 		
 		m_font = tkFont.Font(root=self.result_frame, size=14)
-		self.scrollbar = ttk.Scrollbar(self.result_frame)
-		self.u_scrollbar = ttk.Scrollbar(self.result_frame, orient='horizontal')
-		self.result = tk.Listbox(self.result_frame, bg='grey', width=100, height=20)
-		self.result.config(xscrollcommand = self.u_scrollbar.set,
-			yscrollcommand=self.scrollbar.set, font=m_font)
-		self.result.bind("<Button-3>", self.r_click) #
-		self.scrollbar.config(command=self.result.yview)
-		self.u_scrollbar.config(command=self.result.xview)
+		#self.scrollbar = ttk.Scrollbar(self.result_frame)
+		#self.u_scrollbar = ttk.Scrollbar(self.result_frame, orient='horizontal')
+		self.result = tk.Listbox(self.login_frame, bg='grey', width=50, height=2)
+		self.result.config(font=m_font)
+		# Right-click
 		
-		self.clr_btn = ttk.Button(self.bottom_frame, text="Clear", command=clear_text)
-		self.quit_button = ttk.Button(self.bottom_frame, text="QUIT", command=root.quit)
+		#self.scrollbar.config(command=self.result.yview)
+		#self.u_scrollbar.config(command=self.result.xview)
+		# Treeview
+		self.tree = ttk.Treeview(self.result_frame, height=15, \
+			selectmode="extended")
+		self.tree["columns"] = ("one", "two", "three")
+		self.tree.column("#0", width=50)
+		self.tree.column("one", width=120)
+		self.tree.column("two", width=900)
+		self.tree.column("three", width=500)
 
+		self.tree.heading("one", text="IV Number")
+		self.tree.heading("two", text="Filename")
+		self.tree.heading("three", text="Notes")
+		self.tree_scrollbar = ttk.Scrollbar(self.result_frame, \
+			command=self.tree.yview)
+		self.tree.configure(yscrollcommand=self.tree_scrollbar.set)
+		
+		self.clr_btn = ttk.Button(self.bottom_frame, text="Clear", \
+			command=clear_text)
+		self.quit_button = ttk.Button(self.bottom_frame, text="QUIT", \
+			command=root.quit)
 
+#		self.tree.bind("<Button-2>", self.rc_pop)#self.r_click)
+#		self.rc.bind("<Button-1>", select_all)
 
 	def grid_widgets(self):
 		self.usrname_label.grid(row=0, column=0)
@@ -270,15 +301,33 @@ class QS(tk.Frame):
 
 		self.clip.grid(row=0, column=0, columnspan=4, sticky=E, padx=2) #colspan
 		self.search_btn.grid(row=0, column=5, sticky=E, padx=2)
-		self.scrollbar.grid(column=5, sticky=N+S+W)
-		self.u_scrollbar.grid(row=1, sticky=E+W+S)
-		self.result.grid(row=0, column=0, columnspan=4, sticky=N+S+E+W)
+		#self.scrollbar.grid(column=5, sticky=N+S+W)
+		#self.u_scrollbar.grid(row=1, sticky=E+W+S)
+		self.result.grid(row=0, column=6, columnspan=2, sticky=E)
+
+		self.tree.grid(row=0, column=0)
+		self.tree_scrollbar.grid(row=0, column=1, sticky=N+S)
 		
 		self.clr_btn.grid(row=0, column=0, sticky=E, pady=2, padx=2)
 		self.quit_button.grid(row=0, column=1, sticky=E, pady=2, padx=2)
 
+#	def rc_pop(self):
+#		pop = RC_Menu(self, self.a, self.b)
+#		try:
+#			pop.tk_popup(event.x_root, event.y_root)
+#		finally:
+#			pop.grab_release()
 
-
+#class RC_Menu(tk.Menu):
+#	"""Menu and options for user right click"""
+#	def __init__(self, master, a, b):
+#		tk.Menu.__init__(self, master, tearoff=0)
+#		self.a = a
+#		self.b = b
+#		self.add_command(label="Select All" command=self.loc)
+#		self.add_command(label="Copy")
+#	def loc(self):
+#		print self.a, self.b
 
 root = tk.Tk()
 root.title('CatDV QuickSearch')
@@ -288,4 +337,4 @@ root.minsize(root.winfo_width(), root.winfo_height())
 app = QS(root)
 
 root.mainloop()
-#root.destroy()
+app.destroy()
