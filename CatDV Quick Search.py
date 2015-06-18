4d21ca28137ff970ab593f6e3014911190b6f16c
sp00f <e.cudjoe@hotmail.co.uk>
2015-06-18 12:22:56 +0100

    added sort function to the results columns. removed unneccesary comments

--- CatDV Quick Search.py
+++ CatDV Quick Search.py
@@ -69,16 +69,12 @@ def query():
 						if i['notes']:
 							s_results.append((i['userFields']['U7'], \
 								i['name'], i['notes']))
-#							app.result.insert(END, i['userFields']['U7'] + 
-#								' '*5 + i['name'] + ' '*7 + '*' + i['notes'])
 							app.tree.insert("", count, text=str(count), \
 								values=(i['userFields']['U7'], i['name'], \
 									i['notes']))
 						else:
 							s_results.append((i['userFields']['U7'], \
 								i['name']))       
-#							app.result.insert(END, 
-#								i['userFields']['U7'] + ' '*5 + i['name'])
 							app.tree.insert("", count, text=str(count), \
 								values=(i['userFields']['U7'], i['name']))
 					else:
@@ -139,9 +135,8 @@ def about():
 	tkMessageBox.showinfo("CatDV QuickSearch 1.0b",
 		"\nCatDV QuickSearch\n"
 		"\nCreated by E.Cudjoe"
-		"\nVersion 1.0.1")
-		#"\nCopyright " + "\u00A9" + " 2014-2015 E.cudjoe"
-		#"\nhttps://github.com/edsondudjoe")
+		"\nVersion 1.0.1"
+		"\nhttps://github.com/edsondudjoe")
 
 def select_all(event):
 	tree_items = app.tree.identify_row(event.y)
@@ -159,6 +154,12 @@ W = tk.W
 END = tk.END
 
 class QS(tk.Frame):
+	
+	"""
+	GUI that connects to the CatDV server api and returns clip
+	results based on user entry
+	"""
+	
 	def __init__(self, parent):
 		tk.Frame.__init__(self, parent)
 		self.parent = parent
@@ -182,16 +183,12 @@ class QS(tk.Frame):
 		self.result_frame.columnconfigure(0, weight=4)
 
 		self.create_menubar()
-#		self.create_right_click()
 		self.create_variables()
 		self.create_widgets()
 		self.grid_widgets()
 
 	######## MENU BAR #########
 	def create_menubar(self):
-
-		self.a = 1
-		self.b = 2
 
 		self.menubar = tk.Menu(self.parent)
 		self.filemenu = tk.Menu(self.menubar, tearoff=0,)
@@ -203,8 +200,7 @@ class QS(tk.Frame):
 		self.filemenu.add_command(label="Search", command=query)
 		# File > Export
 		self.exportmenu = tk.Menu(self.filemenu, tearoff=0)
-		self.filemenu.add_cascade(label="Export", menu=self.exportmenu)
-		#self.exportmenu.add_command(label="as Spreadsheet")
+		self.filemenu.add_cascade(label="Export results", menu=self.exportmenu)
 		self.exportmenu.add_command(label="as Text", command=export_text)
 
 		# File
@@ -213,28 +209,10 @@ class QS(tk.Frame):
 		self.filemenu.add_command(label="Logout", command=delete_session)
 		self.filemenu.add_command(label="Quit", command=root.quit)
 
-		# Edit
-		#self.editmenu = tk.Menu(self.menubar, tearoff=0)
-		#self.menubar.add_cascade(label="Edit", menu=self.editmenu)
-		#self.editmenu.add_command(label="Select All", state="disabled")
-		#self.editmenu.add_command(label="Copy", state="disabled")
-
 		# Help
 		self.helpmenu = tk.Menu(self.menubar, tearoff=0)
 		self.menubar.add_cascade(label="Help", menu=self.helpmenu)
 		self.helpmenu.add_command(label="About", command=about)
-
-#	def create_right_click(self):
-#		# Right-click menu
-#		self.rc = tk.Menu(self.parent, tearoff=0)
-#		self.rc.add_command(label="Select All", command=select_all)
-#		self.rc.add_command(label="Copy")
-#
-#	def r_click(self, event):
-#		try:
-#			self.rc.tk_popup(event.x_root, event.y_root, 0)
-#		finally:
-#			self.rc.grab_release()	
 
 	def create_variables(self):
 		self.username = tk.StringVar() 
@@ -263,40 +241,36 @@ class QS(tk.Frame):
 		self.search_btn.bind("<Return>", search_btn_return)
 		
 		m_font = tkFont.Font(root=self.result_frame, size=14)
-		#self.scrollbar = ttk.Scrollbar(self.result_frame)
-		#self.u_scrollbar = ttk.Scrollbar(self.result_frame, orient='horizontal')
 		self.result = tk.Listbox(self.login_frame, bg='grey', width=50, height=2)
 		self.result.config(font=m_font)
-		# Right-click
-		
-		#self.scrollbar.config(command=self.result.yview)
-		#self.u_scrollbar.config(command=self.result.xview)
-		# Treeview
-		self.tree = ttk.Treeview(self.result_frame, height=15, \
-			selectmode="extended")
-		self.tree["columns"] = ("one", "two", "three")
-		self.tree.column("#0", width=50)
-		self.tree.column("one", width=120)
-		self.tree.column("two", width=900)
-		self.tree.column("three", width=500)
 
-		self.tree.heading("one", text="IV Number")
-		self.tree.heading("two", text="Filename")
-		self.tree.heading("three", text="Notes")
+		# Treeview
+		self.columns = ('IV Number', 'Filename', 'Notes')
+		self.tree = ttk.Treeview(self.result_frame, columns=self.columns, \
+			height=15, selectmode="browse")
+		for col in self.columns:
+			self.tree.heading(col, text=col)
+
+		self.tree.column("#0", width=50)
+		self.tree.column("IV Number", width=120)
+		self.tree.column("Filename", width=900)
+		self.tree.column("Notes", width=500)
+
+		self.tree.heading("IV Number", command=lambda: \
+			self.treeview_sort(self.tree, "IV Number", False))
+		self.tree.heading("Filename", command=lambda: \
+			self.treeview_sort(self.tree, "Filename", False))
+		self.tree.heading("Notes", command=lambda: \
+			self.treeview_sort(self.tree, "Notes", False))
+
 		self.tree_scrollbar = ttk.Scrollbar(self.result_frame, \
 			command=self.tree.yview)
 		self.tree.configure(yscrollcommand=self.tree_scrollbar.set)
 		
-#		for i in range(10):
-#			self.tree.insert("", "end", text="Item %s" % i)
-
 		self.clr_btn = ttk.Button(self.bottom_frame, text="Clear", \
 			command=clear_text)
 		self.quit_button = ttk.Button(self.bottom_frame, text="QUIT", \
 			command=root.quit)
-
-#		self.tree.bind("<Button-2>", self.rc_pop)#self.r_click)
-#		self.rc.bind("<Button-1>", select_all)
 
 	def grid_widgets(self):
 		self.usrname_label.grid(row=0, column=0)
@@ -306,10 +280,8 @@ class QS(tk.Frame):
 		self.login_btn.grid(row=0, column=4, padx=2)
 		self.logout_btn.grid(row=0, column=5, padx=2)
 
-		self.clip.grid(row=0, column=0, columnspan=4, sticky=E, padx=2) #colspan
+		self.clip.grid(row=0, column=0, columnspan=4, sticky=E, padx=2)
 		self.search_btn.grid(row=0, column=5, sticky=E, padx=2)
-		#self.scrollbar.grid(column=5, sticky=N+S+W)
-		#self.u_scrollbar.grid(row=1, sticky=E+W+S)
 		self.result.grid(row=0, column=6, columnspan=2, sticky=E)
 
 		self.tree.grid(row=0, column=0)
@@ -318,24 +290,17 @@ class QS(tk.Frame):
 #		self.clr_btn.grid(row=0, column=0, sticky=E, pady=2, padx=2)
 		self.quit_button.grid(row=0, column=1, sticky=E, pady=2, padx=2)
 
-	def rc_pop(self, event):
-		item_iid = self.tree.identify_row(event.y)
-		pop = Right_Click_Menu(self, item_iid)
-		try:
-			pop.tk_popup(event.x_root, event.y_root)	
-			#print(self.tree.item(item_iid, "values"))
-		finally:
-			pop.grab_release()
+	def treeview_sort(self, tv, col, reverse):
+		l = [(tv.set(k, col), k) for k in tv.get_children('')]
+		l.sort(reverse=reverse)
 
-class Right_Click_Menu(tk.Menu):
-	"""Menu and options for user right click"""
-	def __init__(self, master, a):
-		tk.Menu.__init__(self, master, tearoff=0)
-		self.a = a
-		self.add_command(label="Select All", command=self.loc)
-		self.add_command(label="Copy")
-	def loc(self):
-		print (self.a, app.tree.item(self.a, "values"))
+		# rearrange items in sorted positions
+		for index, (val, k) in enumerate(l):
+			tv.move(k, '', index)
+
+		# reverse sort next time
+		tv.heading(col, command=lambda: \
+			self.treeview_sort(tv, col, not reverse))
 
 root = tk.Tk()
 root.title('CatDV QuickSearch')
